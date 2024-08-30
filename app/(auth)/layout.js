c0fd71b65970742882c47d3ae0b9f724b2580c29
Abdiel Otzoy <abdielotzoy@gmail.com>
2024-08-29 22:41:13 -0600

    estructura root

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Layout = ({ children }) => {
   return (
     <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
@@ -5,23 +7,19 @@ const Layout = ({ children }) => {
 
       <nav className="flex justify-between items-center p-4 bg-primary-500 text-white bg-primary-100">
         <div className="flex items-center space-x-4">
-          <a href="/" className="font-bold">
-            Home
-          </a>
-          <a href="/about" className="font-bold px-4">
-            About
-          </a>
-        </div>
-        <div>
-          <a href="/login" className="font-bold px-4">
+          <Link
+            href="/login"
+            className="text-white hover:text-primary-500 font-bold text-2xl p-2"
+          >
+            {" "}
             Login
-          </a>
-          <a href="/register" className="font-bold px-4">
+          </Link>
+          <Link
+            href="/register"
+            className="text-white hover:text-primary-500 font-bold text-2xl p-2"
+          >
             Register
-          </a>
-          <a href="/forgoten" className="font-bold px-4">
-            Forgoten
-          </a>
+          </Link>
         </div>
       </nav>
       {children}
