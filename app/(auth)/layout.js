de2c6f22fdc474eec9c2277977dfe051c085d11c
Diego Alessandro Constanza Padilla <3875424500101@ingenieria.usac.edu.gt>
2024-09-01 00:27:04 -0600

    Comienzo de NavBar en root

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -3,8 +3,6 @@ import Link from "next/link";
 const Layout = ({ children }) => {
   return (
     <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
-      {/* add nav */}
-
       <nav className="flex justify-between items-center p-4 bg-primary-500 text-white bg-primary-100">
         <div className="flex items-center space-x-4">
           <Link href="/login" className="text-white font-bold text-2xl p-2">
