f0841374f366de12f71c7c6d3a14ba11a57bfa53
Diego Alessandro Constanza Padilla <3875424500101@ingenieria.usac.edu.gt>
2024-09-11 01:26:29 -0600

    Parte grafica en el root

--- app/(root)/course/page.js
+++ app/(root)/course/page.js
@@ -1,7 +1,24 @@
 import React from "react";
+import CourseCard from "@/components/shared/CourseCard";
+import { getUserLogged } from "@/lib/actions/user.actions";
 
-const Courses = () => {
-  return <div>Courses</div>;
+const Courses = async () => {
+  const user = await getUserLogged();
+
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center py-5">
+        Bienvenido a los cursos {user?.nombres}! 👋
+      </h1>
+      
+      {/* courses */}
+      <div className="flex flex-col space-y-5 mt-4 w-full">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+  </main>
+);
 };
 
 export default Courses;
--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -7,7 +7,7 @@ import { logout } from "@/lib/actions/user.actions";
 const Layout = ({ children }) => {
   return (
     <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
-      <nav className="flex justify-between items-center p-6 bg-primary-500 text-white bg-primary-300">
+      <nav className="flex justify-between items-center p-6 bg-primary-500 text-white bg-primary-100 mb-5">
         <div className="flex items-center space-x-4">
           <Link
             href="/"
@@ -47,6 +47,9 @@ const Layout = ({ children }) => {
         </div>
       </nav>
       {children}
+      <footer className="flex justify-end items-center p-6 bg-primary-500 text-white bg-primary-100 mt-14">
+        <p className="text-center text-white font-bold text-2xl">RatingUsac</p>
+      </footer>
     </div>
   );
 };
