69f96bc532f274e22e1347e9a26c54f644a87755
Diego Alessandro Constanza Padilla <3875424500101@ingenieria.usac.edu.gt>
2024-09-12 00:50:03 -0600

    Finalizacion grafica

--- components/shared/CourseCard.jsx
+++ components/shared/CourseCard.jsx
@@ -1,43 +1,26 @@
 import React from "react";
 
-const CourseCard = ( /*course*/ ) => {
-    // course = course._doc;
-    //const [approved, setApproved] = React.useState(false);
-
-    const handleAprove = () => {
-        setApproved(!approved);
-    };
-
-    return (
-        <div className="flex justify-center items-center w-full py-2">
-          <div className="max-w-screen-lg w-1/4 bg-secondary-300 shadow-xl rounded-2xl p-5 gap-5 border transition-all ease-in-out duration-500 transform hover:scale-105">
-
-            <h1 className="text-2xl font-bold text-white mb-4">Nombre del curso</h1>
-            
-            {/* Sección de aprobado/no aprobado */}
-            <div className="form-check flex items-center gap-3">
-              <input
-                type="checkbox"
-                className="form-check-input w-6 h-6 rounded"
-                name="approved"
-                id="approved"
-                value="checkedValue"
-              />
-              <label htmlFor="approved" className="text-lg font-semibold text-black">
-                No Aprobado
-              </label>
-            </div>
-      
-            {/* Botón adicional para confirmar */}
-            <div className="mt-6 flex justify-center">
-              <button className="bg-teal-700 text-white font-semibold py-2 px-6 rounded-lg shadow-lg hover:bg-teal-600 hover:shadow-lg transition duration-300">
-                Confirmar
-              </button>
-            </div>
-          </div>
-        </div>
-    );      
+// CourseCard.js
+const CourseCard = ({ valor, label }) => {
+  return (
+    <div className="border border-gray-200 py-4 px-6 rounded-xl shadow-xl flex flex-col items-start bg-white">
+      <h2 className="text-xl font-semibold mb-2 text-center">{label}</h2>
+      <p className="text-gray-700 mb-4">{valor}</p>
+      <p className="text-gray-700 mb-4">Numero de creditos: </p>
+      <div className="flex items-center space-x-2">
+        <input
+          type="checkbox"
+          id="aprobado"
+          name="Aprobado"
+          value="Aprobado"
+          className="w-5 h-5 text-primary-300 border-gray-300 rounded focus:ring-primary-300 appearance-none checked:bg-primary-300 checked:border-transparent transition duration-200 ease-in-out hover:cursor-pointer hover:border-primary-500 border-2"
+        />
+        <label htmlFor="aprobado" className="text-gray-800 font-medium cursor-pointer">
+          Aprobado
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default CourseCard;
-    
