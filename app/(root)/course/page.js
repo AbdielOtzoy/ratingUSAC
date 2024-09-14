7b1758549acda948f84baa254e3360edbd1d90ea
Abdiel Otzoy <abdielotzoy@gmail.com>
2024-09-13 23:02:10 -0600

    adding courses

--- app/(root)/course/page.js
+++ app/(root)/course/page.js
@@ -1,28 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import CourseCard from "@/components/shared/CourseCard";
-import { getUserLogged } from "@/lib/actions/user.actions";
-import {cursos} from "@/constants";
+import { cursos } from "@/constants";
+import { addCoursesToUser } from "@/lib/actions/user.actions";
 
-const Courses = async () => {
-  const user = await getUserLogged();
+const Courses = () => {
+  const [cursosSeleccionados, setCursosSeleccionados] = useState([]);
+
+  const handleSave = async () => {
+    console.log(cursosSeleccionados);
+
+    // send to database select courses and update user
+    const res = await addCoursesToUser(cursosSeleccionados);
+    console.log(res);
+  };
 
   return (
     <main className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-center py-10 px-16">
-        Bienvenido a los cursos {user?.nombres}! 👋
+        Bienvenido a los cursos👋
       </h1>
-      
+
       {/* Renderiza los cursos */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 w-full px-16 transition-all ease-in-out duration-500">
         {cursos.map((curso) => (
-          <CourseCard key={curso.value} value={curso.value} label={curso.label} />
+          <CourseCard
+            key={curso.value}
+            value={curso.value}
+            label={curso.label}
+            onChange={(value, checked) => {
+              if (checked) {
+                setCursosSeleccionados([...cursosSeleccionados, value]);
+              } else {
+                setCursosSeleccionados(
+                  cursosSeleccionados.filter((curso) => curso !== value)
+                );
+              }
+            }}
+            type="edit"
+          />
         ))}
       </div>
-      <button className="bg-primary-500 text-white font-semibold mt-10 py-2 px-4 rounded-lg shadow-lg bg-secondary-100 hover:bg-secondary-200 transition duration-300 ease-in-out w-48">
-          Guardar
+      <button
+        className="bg-primary-500 text-white font-semibold mt-10 py-2 px-4 rounded-lg shadow-lg bg-secondary-100 hover:bg-secondary-200 transition duration-300 ease-in-out w-48"
+        onClick={handleSave}
+      >
+        Guardar
       </button>
-  </main>
-);
+    </main>
+  );
 };
 
 export default Courses;
